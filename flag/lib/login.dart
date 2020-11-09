02dccd2e40bddbbcc392df9a0b12532e7bf882b4
guokaifeng <gkf442573575@163.com>
2020-11-09 17:32:58 +0800

    阴影无边框输入框

--- flag/lib/login.dart
+++ flag/lib/login.dart
@@ -31,8 +31,8 @@ class _LoginState extends State<Login> {
   loginContainer() {
     final Color purpleColor = Colors.deepPurpleAccent;
     return Container(
-        color: Colors.white,
-        width: 350,
+        color: Color.fromRGBO(246, 249, 250, 1),
+        width: 390,
         height: 450,
         alignment: Alignment.center,
         padding: const EdgeInsets.all(20.0),
@@ -48,22 +48,58 @@ class _LoginState extends State<Login> {
                         TextStyle(fontSize: 20, color: purpleColor, height: 1),
                   ),
                 ),
+                // 实现一个阴影 无边框 TextFormField
+                Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        // labelText: '用户名',
+                        hintText: '用户名或邮箱',
+                        labelStyle: TextStyle(color: purpleColor),
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: purpleColor,
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 4.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none)),
+                    validator: (v) {
+                      // 校验用户名
+                      return v.trim().length > 0 ? null : '用户名不能为空';
+                    },
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(218, 232, 237, 0.52),
+                          blurRadius: 5.0,
+                        ),
+                      ]),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
                 TextFormField(
                   decoration: InputDecoration(
-                      labelText: '用户名',
-                      hintText: '用户名或邮箱',
+                      labelText: '密码',
+                      hintText: '请输入密码',
                       labelStyle: TextStyle(color: purpleColor),
                       prefixIcon: Icon(
-                        Icons.person,
+                        Icons.lock,
                         color: purpleColor,
                       ),
                       focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: purpleColor))),
                   validator: (v) {
                     // 校验用户名
-                    return v.trim().length > 0 ? null : '用户名不能为空';
+                    return v.trim().length > 0 ? null : '密码不能为空';
                   },
-                )
+                  obscureText: true,
+                ),
               ],
             )));
   }
