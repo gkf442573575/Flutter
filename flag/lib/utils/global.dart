1c2e1335a7e555afd7b7335267bb91604cbb7dcc
guokaifeng <gkf442573585@163.com>
2021-02-12 21:17:19 +0800

    update

--- flag/lib/utils/global.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Global {
-  static const primaryColor = Color.fromRGBO(74, 104, 250, 1);
-  static const bgColor = Color.fromRGBO(246, 249, 250, 1);
-}
